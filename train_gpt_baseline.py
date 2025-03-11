48d4083ea9f7825a23f59919856dc753b6057397
sampan26 <rvbh87m@gmail.com>
2025-03-10 23:53:13 -0500

    added cuz bruh

--- train_gpt_baseline.py
+++ train_gpt_baseline.py
@@ -279,7 +279,7 @@ raw_model = model.module if ddp else model # always contains the "raw" unwrapped
 max_lr = 6e-4
 min_lr = max_lr * 0.1
 warmup_steps = 715
-max_steps = 19073 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+max_steps = 38146 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
     if it < warmup_steps:
